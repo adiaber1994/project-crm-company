14bfb0bf799fb770910ae985fb821f03e0666b33
adiaber1994 <106983011+adiaber1994@users.noreply.github.com>
2023-04-15 20:40:40 +0300

    routong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupPageComponent } from './auth/signup-page/signup-page.component';
+import { LoginPageComponent } from './home/login-page/login-page.component';
 
 const routes: Routes = [
+  {path: 'home', component: LoginPageComponent},
   {path: 'signup', component: SignupPageComponent}
 ];
 
